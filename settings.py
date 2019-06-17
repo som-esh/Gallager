d8bc3803b79bbb564db3c0f83d5dda3cc1ec8402
som-esh <39019026+som-esh@users.noreply.github.com>
2019-06-17 22:14:47 +0530

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (200,230,230)
+        self.ship_speed_factor = 1.5
